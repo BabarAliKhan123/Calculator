ea484412257da59438dc5b9fab8ea09d021441d8
BabarAliKhan123 <61537976+BabarAliKhan123@users.noreply.github.com>
2020-11-05 15:54:31 +0500

    Calculator

---
+++ Calculatorr.js
@@ -0,0 +1,21 @@
+﻿
+
+function GetNumberonTextbox(num) {
+    var result = document.getElementById("Text1")
+    result.value += num
+}
+
+
+function cleartextbox(){
+    var clear = document.getElementById("Text1")
+    clear.value = ""
+}
+
+function getresult(){
+    var answer = document.getElementById("Text1")
+    answer.value = eval(answer.value)
+}
+
+function removelast() {
+    Text1.value = Text1.value.substring(0, Text1.value.length - 1);
+}
